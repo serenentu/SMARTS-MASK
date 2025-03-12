a570e07200b4d20c47d5470510022c94c5d5339e
jing han <jinghan9921@gmail.com>
2025-03-12 16:39:07 +0800

    revert

--- app/imagereader.tsx
+++ app/imagereader.tsx
@@ -4,7 +4,7 @@ import Button from '../assets/Components/Button';
 import ImageViewer from '../assets/Components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
-test text
+
 
 const PlaceholderImage = require('../assets/images/background-image.png');
 
